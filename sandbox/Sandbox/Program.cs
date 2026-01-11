e00af1733db66e2a7798516ec9eb147dceca94f6
ichi-seki <aka24005@byui.edu>
2026-01-10 22:37:57 -0700

    homework

--- sandbox/Sandbox/Program.cs
+++ sandbox/Sandbox/Program.cs
@@ -4,6 +4,32 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Sandbox World!");
-    }
+        Console.Write("Enter your grade persentage: ");
+        string grade = Console.ReadLine();
+        int persentage = int.Parse(grade);
+        string letter = "";
+
+        if (persentage >= 90)
+        {
+            letter = "A";
+        }
+        else if (persentage >= 80)
+        {
+            letter = "B";
+        }
+        else if (persentage >= 70)
+        {
+            letter = "C";
+        }
+        else if (persentage >= 60)
+        {
+            letter = "D";
+        }
+        else
+        {
+            letter = "F";
+        }
+
+        Console.Write($"Your grade is {letter}");
+}
 }
