591d5279e13c776c4141bcc9a5722b9215be0515
ichi-seki <aka24005@byui.edu>
2026-01-22 21:49:31 -0700

    prepare02

--- prepare/Learning02/Program.cs
+++ prepare/Learning02/Program.cs
@@ -4,6 +4,23 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning02 World!");
+        Job job1 = new Job();
+        job1._jobTitle = "Software Engineer";
+        job1._company = "Apple";
+        job1._startYear = 2022;
+        job1._endYear = 2024;
+
+        Job job2 = new Job();
+        job2._jobTitle = "Backend Developer";
+        job2._company = "Amazon";
+        job2._startYear = 2024;
+        job2._endYear = 2025;
+
+        Resume theResume = new Resume();
+        theResume._name = "John Rock";
+        theResume._job.Add(job1);
+        theResume._job.Add(job2);
+
+        theResume.Display();
     }
 }
